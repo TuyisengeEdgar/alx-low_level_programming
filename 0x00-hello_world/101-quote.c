32dbf306f42db1bd103f91188420cd2750de157f
TuyisengeEdgar <tuyisengeedgar@gmail.com>
2022-06-03 01:24:08 -0700

    add write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
-#include <unistd.h>
+#include <stdio.h>
 /**
- * main - Prints out the last part of a quote in the standard error.
- * Return: Always (1) for error
+ * main - Prints Dora Korpa
+ * Return: Always 1.
  */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, \2015-10-19\n", 59);
-		return (1);
+write(2, "and that piece of art is useful \" - Dora Korpar, 2015-10-19 \n"
+	, 59);
+return (1);
 }
