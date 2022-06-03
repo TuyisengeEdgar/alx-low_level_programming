70f9275a4c2050af171af7f68f1f0db8800e07eb
TuyisengeEdgar <tuyisengeedgar@gmail.com>
2022-06-03 00:06:35 -0700

    add with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point programming
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle, followed by a new line");
+	return (0);
+}	
