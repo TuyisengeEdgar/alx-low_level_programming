b3c938b95f915ff474d3396b5815d29700a49443
TuyisengeEdgar <tuyisengeedgar@gmail.com>
2022-06-08 14:20:38 -0700

    change

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,21 +1,5 @@
 #include<stdio.h>
-/**
- * putchar prints _putchar, followed by a new line.
- * function of 0-putchar.c
- */
-void putchar(void)
-{
-	/*beginning*/
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
-}
+
 /**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
