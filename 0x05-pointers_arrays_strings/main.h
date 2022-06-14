6789de2403a43526a7e677908cbf2c34ec7416fb
TuyisengeEdgar <tuyisengeedgar@gmail.com>
2022-06-14 19:49:26 +0200

    task3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,9 +7,9 @@
  */
 
 #include <stdlib.h>
-#include <string.h>>
+#include <string.h>
 
-int _putchar(char c);
+int _putchar(char a);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 size_t _strlen(const char *str);
