aa0f1370204e7c59c38306ecd0fa46e272abb512
TuyisengeEdgar <tuyisengeedgar@gmail.com>
2022-06-16 14:34:08 +0200

    saves

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,22 +1,41 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strncpy - Entry point
- * @dest: copy to
- * @src: copy from
- * @n: input number of char
- * Return: Always 0 (Success)
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-
-char *_strncpy(char *dest, char *src, int n)
+int main(void)
 {
+	char s1[98];
+	char *p;
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
-		if (i < n)
-			dest[i] = src[i];
-		while (i < n)
-			dest[i++] = '\0';
-
-	return (dest);
+	for (i = 0; i < 98 - 1; i++)
+	{
+		s1[i] = '*';
+	}
+	s1[i] = '\0';
+	printf("%s\n", s1);
+	p = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
+	printf("%s\n", s1);
+	printf("%s\n", p);
+	p = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+	printf("%s", s1);
+	printf("%s", p);
+	for (i = 0; i < 98; i++)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", s1[i]);
+	}
+	printf("\n");
+	return (0);
 }
